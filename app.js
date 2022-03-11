5e25c2708354754a105b3ba960b3624a6f4d8030
selena chosgo <selena.fadu@gmail.com>
2022-03-10 22:24:33 -0300

    subi mercado liebre

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require ('express')
+const app = express()
+const path = require('path')
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+/* rutas */
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'))
+})
+
+app.listen(PORT, function() {
+    console.log(`
+    Servidor escuchando en el puerto ${PORT}
+    http://localhost:${PORT}
+    `)
+})
